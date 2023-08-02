854489480e82475846cc97d45b2f2bf78fa65d56
Arjun Deshraj <arjun.deshraj@gmail.com>
2023-08-02 14:28:18 +0530

    Added backend code to set up sockets and cors for interaction with frontend

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+const http = require('http')
+const express = require('express')
+const cors = require('cors')
+const { Server } = require('socket.io')
+
+const app = express()
+
+app.use(cors())
+
+const io = new Server(server, {
+    cors: {
+        origin: 'http:localhost:3000'
+    }
+})
+
+io.on('Connect', (socket) => {
+    console.log(`User Connected ${socket.id}`)
+
+    socket.on('joinRoom', (data) => {
+        socket.join(data)
+        console.log(`User with ID ${socket.id} joined Room ${data}`)
+    })
+
+    socket.on('sendMessage', (data) => {
+        socket.to(data.room).emit('receiveMessage', data)
+    })
+
+    socket.on('Disconnect', () => {
+        console.log('User Disconnected', socket.id)
+    })
+})
+
+server.listen(8000, () => {
+    console.log('Backend Server Running on PORT 8000')
+}
